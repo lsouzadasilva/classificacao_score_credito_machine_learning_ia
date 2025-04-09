cfaaf03129a700a1f1a567f9003c64b2f00ebcbb
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-09 00:23:23 -0300

    Update app_analise_credito.py

--- app_analise_credito.py
+++ app_analise_credito.py
@@ -23,7 +23,7 @@ def config__tela():
         <a href="https://www.linkedin.com/in/leandro-souza-bi/" target="_blank" style="text-decoration: none; outline: none; border: none;">
             <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn">
         </a>
-        <a href="https://github.com/lsouzadasilva" target="_blank" style="text-decoration: none; outline: none; border: none;">
+        <a href="https://github.com/lsouzadasilva/classificacao_score_credito_machine_learning" target="_blank" style="text-decoration: none; outline: none; border: none;">
             <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub">
         </a>
     </h4>
